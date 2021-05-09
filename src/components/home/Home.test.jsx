2828087771b03accfdb063abb9543835a73797c8
Marko Gacic <poslovnogacic@gmail.com>
2021-05-09 18:27:01 +0200

    initial commit

---
+++ src/components/home/Home.test.jsx
@@ -0,0 +1,10 @@
+describe("Home", () => {
+	it("should be true", () => {
+		const foo = true;
+		expect(foo).toBe(true);
+	});
+	it("should be false", () => {
+		const foo = true;
+		expect(foo).toBe(false);
+	});
+});
